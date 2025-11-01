6a34652b9d1dfd26e1bbed698cfbcad83b89f180
sasuniii0 <sasuniwijerathne@gmail.com>
2025-11-01 09:56:20 +0530

    initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black-100">
+      <h1 className="text-4xl font-bold text-pink-800">Welcome to My Portfolio!</h1>
+      <p className="mt-4 text-lg text-gray-600">This is a simple React app styled with Tailwind CSS.</p>
+
+
+    </div>
+  )
+}
+
